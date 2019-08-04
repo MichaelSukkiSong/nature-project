33018973cc851052959c7bed229935758873782c
sa77irak <sa77irak@gmail.com>
2019-08-04 15:41:12 +0900

    added animation on scroll using jquery and animate.css

--- resources/js/script.js
+++ resources/js/script.js
@@ -10,14 +10,47 @@ $(document).ready(function() {
         }
     }, {
       offset: '25%'
-    })
+    });
 
 
 
-    // click button and scroll to dest
+    // click button and scroll to dest (jquery)
     $('.js--scroll-to-photos').click(function() {
         $('html, body').animate({scrollTop: $('.js--photos').offset().top}, 1000);
     });
+
+
+    // animation when scrolling (waypoint + animate.css)
+    $('.js--wp-1').waypoint(function(direction) {
+        $('.js--wp-1').addClass('animated fadeInDown');
+    }, {
+        offset: '80%'
+    });
+
+    $('.js--wp-2').waypoint(function(direction) {
+        $('.js--wp-2').addClass('animated fadeIn');
+    }, {
+        offset: '65%'
+    });
+
+    $('.js--wp-3').waypoint(function(direction) {
+        $('.js--wp-3').addClass('animated fadeIn');
+    }, {
+        offset: '65%'
+    });
+    
+    $('.js--wp-4').waypoint(function(direction) {
+        $('.js--wp-4').addClass('animated fadeIn');
+    }, {
+        offset: '65%'
+    });
+
+    $('.js--wp-5').waypoint(function(direction) {
+        $('.js--wp-5').addClass('animated fadeIn');
+    }, {
+        offset: '65%'
+    });
+
 
 });
 
