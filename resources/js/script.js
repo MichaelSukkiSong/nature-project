adcd235ed04e98e03493a0ac891da28be4031e54
sa77irak <sa77irak@gmail.com>
2019-08-04 13:25:55 +0900

    added document.ready

--- resources/js/script.js
+++ resources/js/script.js
@@ -1,11 +1,20 @@
 
-var waypoints = $('.js--intro').waypoint(function(direction) {
-    if (direction == 'down') {
-        $('nav').addClass('sticky');
-    } else {
-        $('nav').removeClass('sticky');
-    }
-}, {
-  offset: '25%'
-})
+$(document).ready(function() {
+
+    var waypoints = $('.js--intro').waypoint(function(direction) {
+        if (direction == 'down') {
+            $('nav').addClass('sticky');
+        } else {
+            $('nav').removeClass('sticky');
+        }
+    }, {
+      offset: '25%'
+    })
+
+});
+
+
+
+
+
 
